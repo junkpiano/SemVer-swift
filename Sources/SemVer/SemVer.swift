e6eaaf66a0f164a5d70c36021b03d97c67188dfb
Yusuke Ohashi <github@junkpiano.me>
2024-11-07 16:23:44 +0900

    feat: compare prelease

--- Sources/SemVer/SemVer.swift
+++ Sources/SemVer/SemVer.swift
@@ -1,62 +1,84 @@
 import Foundation
 
+/// A struct representing a SemVer version.
 struct SemVer {
     let major: Int
     let minor: Int
     let patch: Int
+    let preRelease: String?
 }
 
+// MARK: - Initializers
+
 extension SemVer {
-    init(_ versionString: String) {
+
+    public init(_ versionString: String) {
         var comps = versionString.components(separatedBy: ".")
-        while(comps.count < 3) {
+        while comps.count < 3 {
             comps.append("0")
         }
-        
+
         self.major = Int(comps[0]) ?? 0
         self.minor = Int(comps[1]) ?? 0
-        self.patch = Int(comps[2]) ?? 0
+
+        let patchComponents = comps[2].components(separatedBy: "-")
+        self.patch = Int(patchComponents[0]) ?? 0
+        if patchComponents.count == 1 {
+            self.preRelease = nil
+        } else {
+            self.preRelease = String(patchComponents[1])
+        }
     }
+
 }
+
+// MARK: - Comparable
 
 extension SemVer: Comparable {
 
     static func == (lhs: SemVer, rhs: SemVer) -> Bool {
-        return lhs.major == rhs.major &&
-               lhs.minor == rhs.minor &&
-               lhs.patch == rhs.patch
+        return lhs.major == rhs.major && lhs.minor == rhs.minor
+            && lhs.patch == rhs.patch && lhs.preRelease == rhs.preRelease
     }
 
     static func < (lhs: SemVer, rhs: SemVer) -> Bool {
-        if(lhs.major < rhs.major) {
+        if lhs.major < rhs.major {
+            return true
+        }
+
+        if lhs.minor < rhs.minor {
+            return true
+        }
+
+        if lhs.patch < rhs.patch {
             return true
         }
         
-        if(lhs.minor < rhs.minor) {
-            return true
+        if let lhsPreRelease = lhs.preRelease, let rhsPreRelease = rhs.preRelease {
+            return lhsPreRelease < rhsPreRelease
+        } else {
+            return rhs.preRelease == nil
         }
-        
-        if(lhs.patch < rhs.patch) {
-            return true
-        }
-        
-        return false
     }
-    
+
     static func > (lhs: SemVer, rhs: SemVer) -> Bool {
-        if(lhs.major > rhs.major) {
+        if lhs.major > rhs.major {
+            return true
+        }
+
+        if lhs.minor > rhs.minor {
+            return true
+        }
+
+        if lhs.patch > rhs.patch {
             return true
         }
         
-        if(lhs.minor > rhs.minor) {
-            return true
+        if let lhsPreRelease = lhs.preRelease, let rhsPreRelease = rhs.preRelease {
+            return lhsPreRelease > rhsPreRelease
+        } else {
+            return lhs.preRelease == nil
         }
-        
-        if(lhs.patch > rhs.patch) {
-            return true
-        }
-        
-        return false
     }
 
 }
--- Tests/SemVerTests/SemVerTests.swift
+++ Tests/SemVerTests/SemVerTests.swift
@@ -1,16 +1,22 @@
-import XCTest
+import Testing
 @testable import SemVer
 
-final class SemVerTests: XCTestCase {
+final class SemVerTests {
+
+    @Test
     func testExample() throws {
-        let semVer1 = SemVer(major: 1, minor: 1, patch: 0)
-        let semVer2 = SemVer(major: 1, minor: 1, patch: 1)
+        let semVer1Alpha = SemVer(major: 1, minor: 1, patch: 0, preRelease: "alpha.1")
+        let semVer1 = SemVer(major: 1, minor: 1, patch: 0, preRelease: nil)
+        let semVer2 = SemVer(major: 1, minor: 1, patch: 0, preRelease: "alpha.2")
         let semVer3 = SemVer("1")
         let semVer4 = SemVer("1")
         let semVer5 = SemVer("0.3.2")
-        XCTAssertTrue(semVer1 < semVer2)
-        XCTAssertFalse(semVer1 < semVer3)
-        XCTAssertEqual(semVer3, semVer4)
-        XCTAssertTrue(semVer4 > semVer5)
+
+        #expect(semVer1Alpha < semVer2)
+        #expect(semVer1Alpha < semVer1)
+        #expect(semVer1Alpha > semVer3)
+        #expect(semVer3 == semVer4)
+        #expect(semVer5 < semVer4)
+        #expect(semVer5 < semVer3)
     }
 }
